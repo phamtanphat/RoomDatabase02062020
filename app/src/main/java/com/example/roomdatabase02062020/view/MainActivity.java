9ae0a7104c1c9d51def029fd89272e7ee0f2900c
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-08-20 19:04:34 +0700

    toggle form

--- app/src/main/java/com/example/roomdatabase02062020/view/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase02062020/view/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.roomdatabase02062020.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.roomdatabase02062020.R;
+import com.example.roomdatabase02062020.interfaces.OnShouldShowForm;
 import com.example.roomdatabase02062020.viewmodel.MainViewModel;
+import com.google.android.material.textfield.TextInputEditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnShouldShowForm {
 
     MainViewModel mainViewModel;
+    TextInputEditText mEdtEn,mEdtVn;
+    Button mBtnCancel,mBtnAdd,mBtnOpen;
+    CardView mCardForm;
+    RecyclerView mRcvWord;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,9 +70,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void mapView() {
+        mBtnAdd = findViewById(R.id.buttonAddWord);
+        mBtnCancel = findViewById(R.id.buttonCancel);
+        mBtnOpen = findViewById(R.id.buttonOpenForm);
+        mRcvWord = findViewById(R.id.recyclerview);
+        mEdtEn = findViewById(R.id.textinputEn);
+        mEdtVn = findViewById(R.id.textinputVn);
+        mCardForm = findViewById(R.id.carViewForm);
+        setDefaultForm();
     }
     private void observer() {
+
     }
     private void event() {
+        mBtnOpen.setOnClickListener(view -> toggleForm(true));
+        mBtnCancel.setOnClickListener(view -> toggleForm(false));
+    }
+
+    @Override
+    public void toggleForm(Boolean shouldShowForm) {
+        if (shouldShowForm){
+            mCardForm.setVisibility(View.VISIBLE);
+            mBtnOpen.setVisibility(View.GONE);
+        }else{
+            mCardForm.setVisibility(View.GONE);
+            mBtnOpen.setVisibility(View.VISIBLE);
+        }
+    }
+    private void setDefaultForm(){
+        mCardForm.setVisibility(View.GONE);
+        mBtnOpen.setVisibility(View.VISIBLE);
     }
 }
